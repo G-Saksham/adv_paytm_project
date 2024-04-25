ccd909a14408c3b8001a9fc63692d99855b05953
Saksham Gupta <f20180674@pilani.bits-pilani.ac.in>
2024-04-25 22:00:26 +0530

    modified

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -6,19 +6,21 @@ interface AppbarProps {
     },
     // TODO: can u figure out what the type should be here?
     onSignin: any,
-    onSignout: any
+    onSignout: any,
+    label?: string,
 }
 
 export const Appbar = ({
     user,
     onSignin,
-    onSignout
+    onSignout,
+    label
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+    return <div className="flex justify-between border-b px-4 border-slate-300 bg-white p-2">
+        <div className="flex flex-col justify-center text-vi font-bold text-3xl px-10">
+            {label || "PayTM"}
         </div>
-        <div className="flex flex-col justify-center pt-2">
+        <div className="flex flex-col justify-center">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
     </div>
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,16 +2,46 @@
 
 import { ReactNode } from "react";
 
+type buttonType = "default" | "submit" | "cancel" | "processing";
+
 interface ButtonProps {
   children: ReactNode;
   onClick: () => void;
+  type?: buttonType
 }
 
-export const Button = ({ onClick, children }: ButtonProps) => {
+export const Button = ({ onClick, children, type }: ButtonProps) => {
+  type = type || "default";
   return (
-    <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
-      {children}
-    </button>
-
+    <div>
+      {type === "default" ? <button 
+        onClick={onClick} 
+        className="bg-vi hover:bg-gray-500 text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2"
+        >
+        {children}
+      </button>: null}
+      {type === "submit" ? 
+        <button 
+          onClick={onClick} 
+          className="bg-vi hover:bg-black text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2"
+        > {children}
+        </button> : null
+      }
+      {type === "cancel" ? 
+        <button 
+          onClick={onClick} 
+          className="bg-gray-400 hover:bg-gray-700 text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2"
+        > {children}
+        </button> : null
+      }
+      {type === "processing" ? 
+        <button 
+          onClick={onClick} 
+          className="bg-yellow-600 hover:bg-gray-500 text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2"
+        > {children}
+        </button> : null
+      }
+    </div>
   );
 };
+
